4a1515716abb0196e31b84e4b441d213e0d97bb1
hj <hj@gmail.com>
2026-06-15 17:42:16 +0530

    feat(feature_app) : new model added

--- featureapp/models.py
+++ featureapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class FeatureModel(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    age = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
