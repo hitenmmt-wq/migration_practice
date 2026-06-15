9ad02985c5c1bbcef20db8da7418c94481ddf997
hj <hj@gmail.com>
2026-06-15 18:22:02 +0530

    added dashboard data

--- demoapp/views.py
+++ demoapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
 
 # Create your views here.
+
+
+def get_dashboard(data):
+    today = timezone.now().date()
+    cate_data = {
+        "data": data,
+        "date": today,
+    }
+    return cate_data
